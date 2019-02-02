6cf65c0700d71d312499eb9f018263e2400664f5
swapnil-tiwari <swapnil_tiwari@outlook.com>
2019-02-02 21:25:06 +0530

    initial commit stayanonymous

--- assets/js/second.js
+++
@@ -1,9 +0,0 @@
-function OpenPop(){
-	document.getElementById("popback").style.top = '0%';
-	document.getElementById("pop").style.top = '50%';
-}
-
-function ClosePop(){
-	document.getElementById("popback").style.top = '-100%';
-	document.getElementById("pop").style.top = '-200%';
-}
